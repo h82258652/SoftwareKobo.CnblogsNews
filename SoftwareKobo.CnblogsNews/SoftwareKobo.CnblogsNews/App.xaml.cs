65c98250447897c89f4103674ae8951fe57a4d41
h82258652 <842053625@qq.com>
2014-11-11 01:27:39 +0800

    移动MainPage到View文件夹

--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/App.xaml.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/App.xaml.cs
@@ -17,6 +17,7 @@ using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // “空白应用程序”模板在 http://go.microsoft.com/fwlink/?LinkId=391641 上有介绍
+using SoftwareKobo.CnblogsNews.View;
 
 namespace SoftwareKobo.CnblogsNews
 {
--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/View/MainPage.xaml.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/View/MainPage.xaml.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=391641 上有介绍
 
-namespace SoftwareKobo.CnblogsNews
+namespace SoftwareKobo.CnblogsNews.View
 {
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
