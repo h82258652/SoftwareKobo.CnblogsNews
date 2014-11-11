17e264b74552b49669987193f103cac122a5f358
h82258652 <842053625@qq.com>
2014-11-11 13:34:12 +0800

    添加对话框后置代码文件

---
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Dialog/JumpPageDialog.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// “内容对话框”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
+
+namespace SoftwareKobo.CnblogsNews.Dialog
+{
+    public sealed partial class JumpPageDialog : ContentDialog
+    {
+        public JumpPageDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        public int? Page
+        {
+            get;
+            private set;
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            int page;
+            int.TryParse(TxtPage.Text, out page);
+            if (page <= 0 || page > 100)
+            {
+                Page = -1;
+            }
+            else
+            {
+                Page = page;
+            }
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void JumpPageDialog_OnOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+        {
+            TxtPage.Focus(FocusState.Programmatic);
+        }
+    }
+}
