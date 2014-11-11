c434d979f1e260141155fbc2c41b0c98b2f564af
h82258652 <842053625@qq.com>
2014-11-11 13:14:47 +0800

    更新MainPageViewModel

--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/ViewModel/MainPageViewModel.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/ViewModel/MainPageViewModel.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Input;
 
 namespace SoftwareKobo.CnblogsNews.ViewModel
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : ViewModelBase
     {
+        private int _currentPage;
+
+        public MainPageViewModel()
+        {
+            CurrentPage = 1;
+        }
+
+        public ICommand BackCommand
+        {
+            get
+            {
+                return new RelayCommand(BackCommandExecute, BackCommandCanExecute);
+            }
+        }
+
+        public int CurrentPage
+        {
+            get
+            {
+                return _currentPage;
+            }
+            set
+            {
+                _currentPage = value;
+                RaisePropertyChanged(() => CurrentPage);
+                RaisePropertyChanged(() => BackCommand);
+            }
+        }
+
+        public ICommand ForwardCommand
+        {
+            get
+            {
+                return new RelayCommand(ForwardCommandExecute, ForwardCommandCanExecute);
+            }
+        }
+
+        public bool BackCommandCanExecute()
+        {
+            return CurrentPage > 1;
+        }
+
+        public void BackCommandExecute()
+        {
+            // TODO
+        }
+
+        public bool ForwardCommandCanExecute()
+        {
+            return CurrentPage < 100;
+        }
+
+        public void ForwardCommandExecute()
+        {
+            // TODO
+        }
     }
 }
