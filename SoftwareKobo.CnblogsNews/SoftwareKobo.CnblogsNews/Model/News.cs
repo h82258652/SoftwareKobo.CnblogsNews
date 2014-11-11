14df3d0c7d2cab5eb9bdcdccf677a40a9e30b1fd
h82258652 <842053625@qq.com>
2014-11-11 13:41:27 +0800

    添加Model

---
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Model/News.cs
@@ -0,0 +1,44 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+
+namespace SoftwareKobo.CnblogsNews.Model
+{
+    public class News : ObservableObject
+    {
+        private string _publishTime;
+
+        private string _title;
+
+        public Uri DetailLink
+        {
+            get;
+            set;
+        }
+
+        public string PublishTime
+        {
+            get
+            {
+                return _publishTime;
+            }
+            set
+            {
+                _publishTime = value;
+                RaisePropertyChanged(() => PublishTime);
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                RaisePropertyChanged(() => Title);
+            }
+        }
+    }
+}
