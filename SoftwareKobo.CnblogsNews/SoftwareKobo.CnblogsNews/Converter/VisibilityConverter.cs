a0f7a9b2a2e148ee4d75fab2fc5039f4f1232794
h82258652 <842053625@qq.com>
2014-11-11 13:21:01 +0800

    添加值转换器

---
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Converter/VisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SoftwareKobo.CnblogsNews.Converter
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool)
+            {
+                return (bool) value ? Visibility.Visible : Visibility.Collapsed;
+            }
+            throw new InvalidCastException("输入的不是布尔类型。");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
