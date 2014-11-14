2b96f934a3f5d7479c5729c75457fb60cbcd8737
h82258652 <842053625@qq.com>
2014-11-14 16:31:28 +0800

    为JumpPageService添加IDialogService接口

--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Service/JumpPageService.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Service/JumpPageService.cs
@@ -1,10 +1,44 @@
-﻿using SoftwareKobo.CnblogsNews.Dialog;
+﻿using GalaSoft.MvvmLight.Views;
+using SoftwareKobo.CnblogsNews.Dialog;
 using System;
 using System.Threading.Tasks;
 
 namespace SoftwareKobo.CnblogsNews.Service
 {
-    public class JumpPageService
+    public partial class JumpPageService:IDialogService
+    {
+        public Task ShowError(Exception error, string title, string buttonText, Action afterHideCallback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowError(string message, string title, string buttonText, Action afterHideCallback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText, Action<bool> afterHideCallback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowMessage(string message, string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ShowMessageBox(string message, string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public partial class JumpPageService
     {
         public async static Task<int?> GetPage()
         {
