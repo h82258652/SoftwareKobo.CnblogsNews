20f63e7ee08a8a8ac96489025476250ba8bc0482
h82258652 <842053625@qq.com>
2014-12-13 22:02:16 +0800

    更新服务接口至1.1 修正无网络状态下获取评论崩溃的bug

--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/View/CommentPage.xaml.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/View/CommentPage.xaml.cs
@@ -1,12 +1,15 @@
 ﻿// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkID=390556 上有介绍
 
+using System;
 using System.Linq;
 using Windows.UI.Xaml;
+using GalaSoft.MvvmLight.Views;
 using SoftwareKobo.CnblogsAPI.Extension;
 using SoftwareKobo.CnblogsAPI.Model;
 using Windows.Phone.UI.Input;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
+using SoftwareKobo.CnblogsNews.Service;
 
 namespace SoftwareKobo.CnblogsNews.View
 {
@@ -56,15 +59,34 @@ namespace SoftwareKobo.CnblogsNews.View
 
             if (news != null)
             {
-                var comments = await news.CommentAsync();
-                var index = 0;
-                var commentsWithIndex = from temp in comments
-                    select new
+                if (NetworkService.IsNetworkAvailable() == false)
+                {
+                    await new DialogService().ShowError("请检查网络连接。", "错误", "关闭", null);
+                }
+                else
+                {
+                    Exception exception = null;
+                    try
                     {
-                        Index=++index,
-                        Comment=temp
-                    };
-                Lv.ItemsSource = commentsWithIndex;
+                        var comments = await news.CommentAsync(1, int.MaxValue);
+                        var index = 0;
+                        var commentsWithIndex = from temp in comments
+                                                select new
+                                                {
+                                                    Index = ++index,
+                                                    Comment = temp
+                                                };
+                        Lv.ItemsSource = commentsWithIndex;
+                    }
+                    catch (Exception ex)
+                    {
+                        exception = ex;
+                    }
+                    if (exception != null)
+                    {
+                        await new DialogService().ShowError(exception, "错误", "关闭", null);
+                    }
+                }
             }
 
             Pb.Visibility = Visibility.Collapsed;
