ff3ac7d1be5954581d1aab207117a1a6c908d942
h82258652 <842053625@qq.com>
2014-12-31 00:21:11 +0800

    优化代码

--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Model/CommentWithIndex.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Model/CommentWithIndex.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SoftwareKobo.CnblogsAPI.Model;
+﻿using SoftwareKobo.CnblogsAPI.Model;
 
 namespace SoftwareKobo.CnblogsNews.Model
 {
--- SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Service/JumpPageService.cs
+++ SoftwareKobo.CnblogsNews/SoftwareKobo.CnblogsNews/Service/JumpPageService.cs
@@ -1,44 +1,10 @@
-﻿using GalaSoft.MvvmLight.Views;
-using SoftwareKobo.CnblogsNews.Dialog;
+﻿using SoftwareKobo.CnblogsNews.Dialog;
 using System;
 using System.Threading.Tasks;
 
 namespace SoftwareKobo.CnblogsNews.Service
 {
-    public partial class JumpPageService:IDialogService
-    {
-        public Task ShowError(Exception error, string title, string buttonText, Action afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ShowError(string message, string title, string buttonText, Action afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> ShowMessage(string message, string title, string buttonConfirmText, string buttonCancelText, Action<bool> afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ShowMessage(string message, string title, string buttonText, Action afterHideCallback)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ShowMessage(string message, string title)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task ShowMessageBox(string message, string title)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public partial class JumpPageService
+    public class JumpPageService
     {
         public async static Task<int?> GetPage()
         {
